1ef4dbb8f0a4a17c0f6acb6ede2639d8ade6f1bc
侯法超 <geekidentity@163.com>
2018-11-23 18:54:18 +0800

    dapp

---
+++ src/main/java/com/dappstore/web/controller/DAppController.java
@@ -0,0 +1,12 @@
+package com.dappstore.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author houfc
+ * @date 2018/11/22
+ */
+@RestController
+public class DAppController {
+
+}
---
+++ src/main/java/com/dappstore/web/entity/Account.java
@@ -0,0 +1,13 @@
+package com.dappstore.web.entity;
+
+import lombok.Data;
+
+/**
+ * @author houfc
+ * @date 2018/11/22
+ */
+@Data
+public class Account {
+    private String account;
+    private String code;
+}
